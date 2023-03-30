d469b64e8a2454e2de9f1d1951633567583c44e1
Klyuev <Klyuev_Andrey@mail.ru>
2023-03-31 00:14:46 +0300

    main page first design added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TuiButtonModule, TuiRootModule} from "@taiga-ui/core";
+import {TuiInputFilesModule, TuiTextAreaModule} from "@taiga-ui/kit";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TuiRootModule,
+    TuiTextAreaModule,
+    TuiInputFilesModule,
+    TuiButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
